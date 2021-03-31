a9c87d211b5d8e8dc8eace24c85b393d1cdde61c
mattkday <mattkday00@gmail.com>
2021-03-31 11:41:20 -0500

    more idea structure stuff

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Tic-Tac-Toe_Project
 
+function = display_board(board) %display board given matrix A
+
+function board = player1_turn(board) %ask for input; check if legal & output matrix of new board 
+
+function (bool) = isGameOver(board) %check if game over
+
+function board = comp_turn(board, iter) %decide next move using if statements for each situation
--- Tic_Tac_Toe_Script.m
+++ Tic_Tac_Toe_Script.m
@@ -3,13 +3,38 @@
 %define vars
 
 for i=1:100
-%ask for input
+%display board given matrix A
+
+
+%ask for input output matrix A
 
 
 %check if game over
 
 
-%decide next move
+%decide next move using function(A, i) and if staements
+
+
+%       Player O must always respond to a corner opening with a center mark, and to a center opening with a corner mark. 
+%       An edge opening must be answered either with a center mark, a corner mark next to the X, or an edge mark opposite the X
+
+
+
+%     Win: If the player has two in a row, they can place a third to get three in a row.
+
+%     Block: If the opponent has two in a row, the player must play the third themselves to block the opponent.
+
+%     Fork: Create an opportunity where the player has two ways to win (two non-blocked lines of 2).
+
+%     Blocking an opponent's fork: If there is only one possible fork for the opponent, the player should block it. Otherwise, the player should block all forks in any way that simultaneously allows them to create two in a row. Otherwise, the player should create a two in a row to force the opponent into defending, as long as it doesn't result in them creating a fork. For example, if "X" has two opposite corners and "O" has the center, "O" must not play a corner move in order to win. (Playing a corner move in this scenario creates a fork for "X" to win.)
+
+%     Center: A player marks the center. (If it is the first move of the game, playing a corner move gives the second player more opportunities to make a mistake and may therefore be the better choice; however, it makes no difference between perfect players.)
+
+%     Opposite corner: If the opponent is in the corner, the player plays the opposite corner.
+
+%     Empty corner: The player plays in a corner square.
+
+%     Empty side: The player plays in a middle square on any of the 4 sides.
 
 
 %check if game over
